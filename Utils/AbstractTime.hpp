7315c8c1aa5cbd0b34e225b3d1b80dcbf6497782
Aaron A. Pabst <aaronp@hyper.earth>
2025-07-23 18:07:00 +0000

    add time interface

--- Utils/AbstractTime.hpp
+++ Utils/AbstractTime.hpp
@@ -8,5 +8,10 @@ namespace Meta {
         virtual uint32_t now(void) {
             return 0; // TODO: Default to ctime
         }
+
+        virtual void delayMs(uint32_t ms) {
+            uint32_t start = now();
+            while (now() - start < ms) {}
+        }
     };
 }
